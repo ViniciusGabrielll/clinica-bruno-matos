40ae392980d98feb94496c960bfa27363681dd4d
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-08-06 10:19:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,155 @@
+import { data } from "./data/data";
+
+import { FaHeart } from "react-icons/fa";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src="" alt="Logo" />
+        <nav>
+          <a>Início</a>
+          <a>Sobre</a>
+          <a>Tratamentos</a>
+          <a>Contato</a>
+        </nav>
+        <a>
+          <img src="" alt="Ícone do Instagram" />
+        </a>
+      </div>
+      <article>
+        <h1>{data.name}</h1>
+        <h2>A maior clínica de estética em {data.city}</h2>
+        <div>
+          <button>Ir para o WhatsApp</button>
+          <button>Ver Tratamentos</button>
+        </div>
+
+        <div>
+          <a>
+            <img src="" alt="Ícone do WhatsApp" />
+          </a>
+          <a>
+            <img src="" alt="Ícone do Instagram" />
+          </a>
+        </div>
+
+        <img src="" alt="Modelo" />
+      </article>
+
+      <article>
+        <h3>SOBRE NÓS</h3>
+        <h2>Titulo Sobre nós</h2>
+        <p>{data.textAboutUs}</p>
+        <button>Ver Tratamentos</button>
+        <div>
+          <div>
+            <h4>{data.differential1Title}</h4>
+            <p>{data.differential1Description}</p>
+          </div>
+        </div>
+      </article>
+
+      <article>
+        <h2>Tratamentos</h2>
+        {data.services.map((service) => (
+          <div>
+            <img src={service.image} alt={service.title} />
+            <h3>{service.title}</h3>
+            <p>{service.description}</p>
+          </div>
+        ))}
+        <button>Ver Todos</button>
+      </article>
+
+      <article>
+        <section>
+          <form>
+            <h3>ENVIE UMA MENSAGEM</h3>
+            <input type="text" placeholder="Nome Completo" />
+            <select name="servico" id="servico">
+              {data.services.map((service) => (
+                <option key={service.title} value={service.title}>{service.title}</option>
+              ))}
+              <option key="outro" value="outro">Outro</option>
+            </select>
+            <textarea placeholder="Mensagem"></textarea>
+            <button type="submit">Enviar</button>
+          </form>
+          <img src="" alt="Modelo" />
+        </section>
+
+        <section>
+          <iframe src={data.mapsLink}></iframe>
+          <div>
+            <h3>ENVIE UMA MENSAGEM</h3>
+            <h2>Saiba mais sobre nós</h2>
+            <p>Agende sua avaliação ou tire suas dúvidas. Nossa equipe está pronta para te atender com atenção e exclusividade.</p>
+            <div>
+              <a href={data.whatsAppLink} target="_blank" rel="noopener noreferrer">
+                <img src="" alt="Ícone do WhatsApp" />
+                <div>
+                  <h5>Telefone / WhatsApp</h5>
+                  <h4>{data.number}</h4>
+                </div>
+              </a>
+              <a href={data.instagramLink} target="_blank" rel="noopener noreferrer">
+                <img src="" alt="Ícone do Instagram" />
+                <div>
+                  <h5>Instagram</h5>
+                  <h4>@{data.instagram}</h4>
+                </div>
+              </a>
+              <div>
+                <img src="" alt="Ícone de Horário" />
+                <div>
+                  <h5>Horário de Funcionamento</h5>
+                  <h4>{data.businessHours}</h4>
+                </div>
+              </div>
+              <div>
+                <img src="" alt="Ícone de Localização" />
+                <div>
+                  <h5>Endereço</h5>
+                  <h4>{data.location}</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </article>
+
+      <footer>
+        <div>
+          <div>
+            <p>{data.textAboutUs}</p>
+          </div>
+          <div>
+            <h3>Links <br /> Rápidos —</h3>
+            <a>Início</a>
+            <a>Sobre</a>
+            <a>Tratamentos</a>
+            <a>Contato</a>
+          </div>
+          <div>
+            <h3>Horário de<br /> Funcionamento —</h3>
+            <p>{data.businessHours}</p>
+          </div>
+          <div>
+            <h3>Fale <br />Conosco —</h3>
+            <a href={data.whatsAppLink} target="_blank" rel="noopener noreferrer">{data.number}</a>
+            <a href={data.instagramLink} target="_blank" rel="noopener noreferrer">@{data.instagram}</a>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="Logo" />
+          <span>© 2026 {data.name}.<br /> Feito com <FaHeart /></span>
+        </div>
+
+      </footer >
+    </>
+  )
+}
+
+export default App
---
+++ src/data/data.ts
@@ -0,0 +1,53 @@
+export const data = {
+    name: "Gerci Oliveira",
+    city: "Moreno",
+    location: "Rua 1 de Maio - 44 Centro, Moreno, Pernambuco, Brazil 54800000",
+    mapsLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.8306390659723!2d-35.101760924612144!3d-8.118720181249042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab03f4022b2697%3A0xdd0b755be16261c8!2sClinica%20de%20Est%C3%A9tica%20Gerci%20Oliveira!5e0!3m2!1spt-BR!2sbr!4v1785963715453!5m2!1spt-BR!2sbr",
+    number: "+55 (81) 9 9267-3609",
+    whatsAppLink: "https://api.whatsapp.com/send?phone=558199267-3609",
+    instagram: "gerci_esteticista",
+    instagramLink: "https://www.instagram.com/gerci_esteticista/",
+    businessHours: "Segunda a sábado: 08:00 às 18:00 Domingos: Fechado",
+
+    textAboutUs: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+
+
+    differential1Title: "+ 8 mil",
+    differential1Description: "procedimentos realizados",
+    differential2Title: "+9",
+    differential2Description: "Anos de experiência",
+    differential3Title: "4,8",
+    differential3Description: "Avaliação no google",
+    differential4Title: "96%",
+    differential4Description: "Satisfação dos clientes",
+
+    services: [
+        {
+            image: "",
+            title: "Design de Sombrancelhas",
+            description: "Modelagem das sobrancelhas de acordo com o formato do seu rosto."
+        },
+        {
+            image: "",
+            title: "Design de Sombrancelhas",
+            description: "Modelagem das sobrancelhas de acordo com o formato do seu rosto."
+        },
+        {
+            image: "",
+            title: "Design de Sombrancelhas",
+            description: "Modelagem das sobrancelhas de acordo com o formato do seu rosto."
+        },
+        {
+            image: "",
+            title: "Design de Sombrancelhas",
+            description: "Modelagem das sobrancelhas de acordo com o formato do seu rosto."
+        },
+        {
+            image: "",
+            title: "Design de Sombrancelhas",
+            description: "Modelagem das sobrancelhas de acordo com o formato do seu rosto."
+        },
+
+    ]
+}
